45e80d43d2319231cf392e456fc75368ec35de94
adam <adam@alandstreet.com>
2025-12-02 08:11:58 +1000

    build dir for github pages

---
+++ docs/assets/use-quasar-B_v9VJYP.js
@@ -0,0 +1,1 @@
+import{n as a,q as r}from"./index-BeZvCyrH.js";function u(){return a(r)}export{u};
--- quasar.config.js
+++ quasar.config.js
@@ -40,7 +40,7 @@ export default defineConfig((/* ctx */) => {
         browser: [ 'es2022', 'firefox115', 'chrome115', 'safari14' ],
         node: 'node20'
       },
-
+      distDir: './docs',
       vueRouterMode: 'history', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
